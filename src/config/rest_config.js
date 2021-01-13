6c5e7280ccd30bf877533112033cab2741017682
ginabagi <415308@student.fontys.nl>
2021-01-13 12:50:45 +0100

    API config

---
+++ src/config/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = 'http://newsapi.org/v2/top-headlines';
+export const country_code = 'nl';
+export const category = 'general';
+export const _api_key = '758f62aec918492b8d710d775b06006f';
