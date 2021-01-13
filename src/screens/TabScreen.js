29ad0aae60f2e4b14c2c345e1ab977839d294e53
ginabagi <415308@student.fontys.nl>
2021-01-13 11:10:29 +0100

    Tabscreen update

--- src/screens/TabScreen.js
+++ src/screens/TabScreen.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Tab, Tabs } from 'native-base';
-import Tab1 from './tabs/tab1';
-import Tab2 from './tabs/tab2';
-import Tab3 from './tabs/tab3';
+import { Container, Header, Content, Tab, Tabs, Left, Body, Right, Title } from 'native-base';
+import Tab1 from '../screens/tabs/tab1';
+import Tab2 from '../screens/tabs/tab2';
+import Tab3 from '../screens/tabs/tab3';
+
 export default class TabsExample extends Component {
   render() {
     return (
       <Container>
-        <Header hasTabs />
+        <Header style={{backgroundColor:'#37897E'}} hasTabs>
+        <Left/>
+          <Body>
+            <Title style={{color:'#FFFFFF'}}>Gin's News App</Title>
+          </Body>
+          <Right/>
+        </Header>
         <Tabs>
-          <Tab heading="tab1">
+          <Tab tabStyle={{backgroundColor:'#37897E'}}  
+          activeTabStyle={{backgroundColor:'#37897E'}} 
+          textStyle={{color:'#FFFFFF'}}
+          activeTextStyle={{color:'#FFFFFF'}} heading="Tab1">
             <Tab1 />
           </Tab>
-          <Tab heading="tab2">
+          <Tab tabStyle={{backgroundColor:'#37897E'}} 
+          activeTabStyle={{backgroundColor:'#37897E'}} 
+          textStyle={{color:'#FFFFFF'}}
+          activeTextStyle={{color:'#FFFFFF'}} heading="Tab2">
             <Tab2 />
           </Tab>
-          <Tab heading="tab3">
+          <Tab tabStyle={{backgroundColor:'#37897E'}} 
+          activeTabStyle={{backgroundColor:'#37897E'}} 
+          textStyle={{color:'#FFFFFF'}}
+          activeTextStyle={{color:'#FFFFFF'}} heading="Tab3">
             <Tab3 />
           </Tab>
         </Tabs>
